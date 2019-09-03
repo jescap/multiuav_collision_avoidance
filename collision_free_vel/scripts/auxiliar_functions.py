616b17668e301810a49b5ee9206bad014e22c6e7
fragnarxx <32336842+fragnarxx@users.noreply.github.com>
2019-09-03 16:57:53 +0200

    Update auxiliar_functions.py

--- collision_free_vel/scripts/auxiliar_functions.py
+++ collision_free_vel/scripts/auxiliar_functions.py
@@ -6,20 +6,19 @@ def vectors_distance_by_components(vector1, vector2):
 
 vect_dist = vectors_distance_by_components
 
-
-# Return vector normalized by euclidean norm
+# Dado un vector devuelve el vector normalizado con norma euclideana
 def get_normalized_vector(vector):
     vx, vy, vz = vector
     n = vector_norm(vector)
     return (vx/n, vy/n, vz/n)
 
 
-# Return vector norm
+# Retorna la norma de un vector
 def vector_norm(vector):
     return sqrt(vector[0]**2 + vector[1]**2 + vector[2]**2)
 
 
-# Distance from point to a line in space
+# Distancia de punto a recta en el espacio
 def rect_point_space_distance(rect_point, rect_vector, point):
 
     p11, p12, p13 = point
@@ -28,7 +27,7 @@ def rect_point_space_distance(rect_point, rect_vector, point):
     q1, q2, q3 = p11 - p21, p12 - p22, p13 - p23
     v1, v2, v3 = rect_vector
 
-    # vectorial product
+    # producto vectorial
     p1, p2, p3 = (q2 * v3 - q3 * v2, -(q1 * v3 - q3 * v1), q1 * v2 - q2 * v1)
 
     s = (v1 ** 2 + v2 ** 2 + v3 ** 2)
@@ -37,6 +36,7 @@ def rect_point_space_distance(rect_point, rect_vector, point):
 
 def vector2angles(vector):
     Vx, Vy, Vz = vector
+    # phi, theta, r
     return atan2(Vy, Vx) % (2 * pi), acos(Vz) % (2 * pi), (Vx**2 + Vy**2 + Vz**2)**0.5
 
 
@@ -44,7 +44,7 @@ def angles2vector(phi, theta = pi/2, r = 1):
     return r*sin(theta)*cos(phi), r*sin(theta)*sin(phi), r*cos(theta)
 
 
-# Verify if there is collision between one UAV and a list of UAVs and directions
+# Verificar si existe colision entre un dron y una lista de drones y direcciones
 def provoke_collisions(UAV, direction, UAV_list, detect_collision_method):
     for uav, d in UAV_list:
         if detect_collision_method(uav, d, UAV, direction):
@@ -52,24 +52,21 @@ def provoke_collisions(UAV, direction, UAV_list, detect_collision_method):
     return False
 
 
-# Detect if UAVs collide following their directions within a time interval
+# Detecta si los UAVs colisionan siguiendo sus direcciones en un intervalo de tiempo
 def is_collision_on_interval(UAV1, d1, UAV2, d2, time_interval):
     UAV1, d1, UAV2, d2 = (UAV1, d1, UAV2, d2) if UAV1.velocity < UAV2.velocity else (UAV2, d2, UAV1, d1)
     distance, vector = colliding_directions(UAV1, d1, UAV2, d2)
 
-    if distance == None: return False
+    if distance == None or distance > (UAV1.radio + UAV2.radio): return False
 
     time_collision = sqrt(distance**2 + vect_dist(UAV1.position, UAV2.position)**2)/vector_norm(vector)
-    time = min(time_collision, time_interval)
+    if time_collision > time_interval:
+        return False
 
-    x, y = UAV2.position[0] + time*vector[0], UAV2.position[1] + time*vector[1]
-
-    if vect_dist((x,y,0), UAV1.position) <= (UAV1.radio + UAV2.radio) and time > 0:
-        return True
-    return False
+    return True
 
 
-# Detect if there are collisions within a time interval no matter the region
+# Detecta si hay colisiones en un intervalo de tiempo sin importar la region
 def detect_collisions_on_time_interval(UAVs, time_interval):
 
     for i in range(len(UAVs)):
@@ -80,11 +77,10 @@ def detect_collisions_on_time_interval(UAVs, time_interval):
     return False
 
 
-# Given two UAVs with two directions, check whether they collide and the direction translated for UAV2
+# Dados los drones UAV1, UAV2 siguiendo dos direcciones saber si colisionan y la direccion trasladada para UAV2
 def colliding_directions(UAV1, d1, UAV2, d2):
-    # leave in UAV1 the one with slowest velocity that was fixed
+    # dejar en UAV1 el de menor velocidad que se fijara
 
     vector = (d2[0]*UAV2.velocity - d1[0]*UAV1.velocity, d2[1]*UAV2.velocity - d1[1]*UAV1.velocity, d2[2]*UAV2.velocity - d1[2]*UAV1.velocity)
     return rect_point_space_distance(UAV2.position, vector, UAV1.position), vector
-
 
