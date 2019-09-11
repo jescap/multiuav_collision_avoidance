2c4a91d868cfe64b6cda6ac8a115be26329264d6
fragnarxx <32336842+fragnarxx@users.noreply.github.com>
2019-09-11 03:05:50 +0200

    Update algorithms.py

--- collision_free_vel/scripts/algorithms.py
+++ collision_free_vel/scripts/algorithms.py
@@ -3,16 +3,16 @@ from auxiliar_functions import provoke_collisions
 
 # Calcula todas las soluciones factibles y escoge la optima segun el criterio de minimizar la maxima desviacion
 def bf_minimize_max_deviation(UAVs, directions_list, cost_function, detect_collision_method):
-
+    
     def criteria(uavs):
         return max([cost_function(uav.direction, d) for uav, d in uavs])
 
     result_list = optimize_brute_force(UAVs, directions_list, 0, [], [], detect_collision_method)
     result_list.sort(key= lambda x: criteria(x))
 
-    result = []
-    for i in range(1,len(result_list)):
-        if criteria(result_list[i]) != criteria(result_list[i-1]):
+    result, value = [result_list[0]], criteria(result_list[0])
+    for i in range(1, len(result_list)):
+        if criteria(result_list[i]) != value:
             break
         result.append(result_list[i])
 
@@ -37,8 +37,13 @@ def optimize_brute_force(UAVs, directions_list, index, result, result_list, dete
 
 
 def select_optimum(result_list, cost):
-    if not len(result_list): 
-        return False
-    costs = list(map(cost, result_list[:]))
-    index = costs.index(min(costs))
-    return result_list[index]
+    if not result_list: return False
+
+    result_list.sort(key=lambda x: cost(x))
+    result, value = [result_list[0]], cost(result_list[0])
+    for i in range(1, len(result_list)):
+        if cost(result_list[i]) != value:
+            break
+        result.append(result_list[i])
+    shuffle(result)
+    return result[0]
